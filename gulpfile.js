8f0f0a83136cc9f24d3ed5fb0f4e3c67846786a4
Jan Ivica <wesleymustdie@gmail.com>
2018-04-16 12:40:24 +0200

    Rework build system.

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,69 @@
 
-const babel   = require("babel-core");
-const rollup  = require("rollup");
-const resolve = require("rollup-plugin-node-resolve");
-const uglify  = require("uglify-js");
-const umd     = require("umd");
+const rollup    = require("rollup")
+const resolve   = require("rollup-plugin-node-resolve")
+const commonjs  = require("rollup-plugin-commonjs")
+const minify    = require("babel-minify")
 
-const package = require("./package.json");
 
 const t = {
 
-   rollup:  (contents, file) => rollup.rollup({ input: file.path, plugins: [resolve()] })
-                                 .then( bundle => bundle.generate({ format: "cjs" }) )
-                                 .then( ({ code }) => code )
-                                 .catch( handleRollupError ),
+   rollup(contents, file){
+      return rollup.rollup({ input: file.path, plugins: [resolve(), commonjs()] })
+            .then( bundle => bundle.generate({ format: "umd", name: "Siteswap" }) )
+            .then( ({ code }) => code )
+            .catch( handleRollupError )
+   },
 
-   uglify:  (contents, file) => { const options = { toplevel: true, mangle: { reserved: ["Siteswap","Juggle","Toss","State","Transition"] }};
-                                  const { code, error } = uglify.minify({ [file.path]: contents.toString() }, options);
-                                  if( error )
-                                    handleUglifyError(error);
-                                  return code; },
+   minify(contents, file){
+      try{
+         return minify(contents, { mangle: { exclude: ["Siteswap"] } }).code
+      }
+      catch(e){
+         handleMinifyError(e)
+      }
+   }
 
-   babel:   (contents, file) => { try {
-                                    return babel.transform(contents, package.babel).code;
-                                  } catch(e){ 
-                                    handleBabelError(e)
-                                  }
-                                },
-
-   umd:     (contents, file) => umd("Siteswap", contents, { commonJS: true })
-
-};
+}
 
 
 const configuration = {
 
   tasks: {
 
-    "build": {
+   "build": {
       watch: "src/*.js",
-      parallel: ["build:js", "build:js:min"]
-    },
-
-    "build:js": {
-      src: "src/entry.js",
+      src: "src/_entry.js",
       rename: "dist/siteswap.js",
-      transforms: [t.rollup, t.umd]
-    },
+      transforms: [t.rollup, t.minify]   
+   },
 
-    "build:js:min": {
-      src: "src/entry.js",
-      rename: "dist/siteswap.min.js",
-      transforms: [t.rollup, t.babel, t.uglify, t.umd]
-    },
-
-    "default": {
+   "default": {
       series: ["build", "watch"]
-    }
+   }
 
   }
 
-};
+}
 
-require("glupost")(configuration);
-
-
+require("glupost")(configuration)
 
 
-// Convert plugin specific errors to a generic form.
+
 
 function handleRollupError({ name, message, loc, frame }){
 
-   let output = "\nRollup errored out\n\n";
-   output += ` ${name}: ${message}\n`;
+   let output = "\nRollup errored out\n\n"
+   output += ` ${name}: ${message}\n`
    if( loc )
-      output += `     at ${loc.file}:${loc.line}:${loc.column}\n`;
+      output += `     at ${loc.file}:${loc.line}:${loc.column}\n`
    if( frame )
-      output += `\n${frame.replace(/^/mg, " ")}\n`;
-   throw output;
+      output += `\n${frame.replace(/^/mg, " ")}\n`
+   throw output
 
 }
 
-function handleBabelError({ name, message, loc, codeFrame: frame }){
+function handleMinifyError({ name, message }){
 
-   let output = "\nBabel errored out\n\n";
-   output += ` ${name}: ${message}\n`;
-   if( frame )
-      output += `\n${frame.trim().replace(/^/mg, " ")}\n`;
-   throw output;
-
-}
-
-function handleUglifyError({ name, message, filename, line, col }){
-
-   let output = "\nUglify errored out\n\n";
-   output += ` ${name}: ${message}\n     at ${filename}:${line}:${col}\n\n`;
-   throw output;
+   throw `\nMinify errored out\n\n ${name}: ${message}\n`
 
 }
